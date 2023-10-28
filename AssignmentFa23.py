29ec6721f39e30f5565c8bb8f557991f7a7834b2
Neeze <60569049+Neeze@users.noreply.github.com>
2023-10-28 21:09:26 +0700

    Code Structure

---
+++ AssignmentFa23.py
@@ -0,0 +1,31 @@
+
+class AirPort:
+    def __init__(self, AirportId: str, AirportName: str):
+        self.AirportId = AirportId
+        self.AirportName = AirportName
+
+
+class AirportManager:
+    def __init__(self):
+        self.CostMatrix: list = [[]]
+
+    def AddAP(self):
+        pass
+
+    def DisplayAP(self):
+        pass
+
+    def SearchAP(self):
+        pass
+
+    def CostCal(self):
+        pass
+
+    def UpdateAP(self):
+        pass
+
+    def DelAP(self):
+        pass
+
+
+A = AirportManager
