d6c2c930d7388914b2d812831cb9bd85a0175bd2
Neeze <60569049+Neeze@users.noreply.github.com>
2023-10-29 21:07:03 +0700

    add function AddAP

--- AssignmentFa23.py
+++ AssignmentFa23.py
@@ -1,18 +1,37 @@
+import random
+
 
 class AirPort:
-    def __init__(self, AirportId: str, AirportName: str):
+    def __init__(self, AirportId: int, AirportName: str):
         self.AirportId = AirportId
         self.AirportName = AirportName
 
 
 class AirportManager:
     def __init__(self):
-        self.CostMatrix: list = [[]]
+        self.CostMatrix: list = []  # Adjacency matrix
+        self._ListAP: set = set()  # Private set contains Airport have been added
+        self._NumberOfAP: int = 0
 
-    def AddAP(self):
-        pass
+    def AddAP(self, APId: int, APName: str):
+        newAirport = AirPort(APId, APName)
+        if APId in self._ListAP:
+            print(f"The airport with ID {APId} existed!")  # Check APId whether exists or not
+        else:
+            self._ListAP.add(APId)
+            adjacencyRow = []
+            for i in range(self._NumberOfAP):
+                adjacencyRow.append(round(random.random() * 100, 2))  # Generate random cost over other AP
+            for row in self.CostMatrix:
+                row.append(round(random.random() * 100, 2))  # Update cost to new AP for each existed AP
+            adjacencyRow.append(newAirport)
+            self.CostMatrix.append(adjacencyRow)  # Add new adjacency row to adjacency matrix
+            self._NumberOfAP += 1
+            print(f"Successfully adding new Airport with ID {APId}!")
 
     def DisplayAP(self):
+        for row in self.CostMatrix:
+            print(row)
         pass
 
     def SearchAP(self):
@@ -28,4 +47,14 @@ class AirportManager:
         pass
 
 
-A = AirportManager
+def main():
+    Manager = AirportManager()
+    Manager.AddAP(0, 'Tuyet Voi')
+    Manager.AddAP(1, 'Tuyet Voi 1')
+    Manager.AddAP(2, 'Tuyet Voi 2')
+    Manager.DisplayAP()
+    pass
+
+
+if __name__ == "__main__":
+    main()
