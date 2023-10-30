9cea3fcc4b60b08bc10fa3fcced7353023372874
Neeze <60569049+Neeze@users.noreply.github.com>
2023-10-30 10:31:09 +0700

    Update and add some attributes

--- AssignmentFa23.py
+++ AssignmentFa23.py
@@ -1,5 +1,5 @@
 import random
-from typing import Union, Any
+from typing import Union
 
 
 class AirPort:
@@ -12,50 +12,46 @@ class AirPort:
 class AirportManager:
     def __init__(self):
         self.CostMatrix: list = []  # Adjacency matrix
-        self._ListAP: set = set()  # Private set contains Airport have been added
+        self._ListAPId: set = set()  # Private set contains Airport have been added
+        self._ListAirports: list = []
         self._NumberOfAP: int = 0
 
     def AddAP(self, APId: int, APName: str):
         newAirport = AirPort(APId, APName)
-        if APId in self._ListAP:
+        if APId in self._ListAPId:
             print(f"The airport with ID {APId} existed!")  # Check APId whether exists or not
         else:
-            self._ListAP.add(APId)
+            self._ListAPId.add(APId)
+            self._ListAirports.append(newAirport)
             adjacencyRow = []
             for i in range(self._NumberOfAP):
                 adjacencyRow.append(round(random.random() * 100, 2))  # Generate random cost over other AP
             for row in self.CostMatrix:
                 row.append(round(random.random() * 100, 2))  # Update cost to new AP for each existed AP
-            adjacencyRow.append(newAirport)
+            adjacencyRow.append(0)
             self.CostMatrix.append(adjacencyRow)  # Add new adjacency row to adjacency matrix
             self._NumberOfAP += 1
             print(f"Successfully adding new Airport with ID {APId}!")
 
-    def DisplayAP(self):
+    def DisplayAP(self):  # Display the matrix
         for row in self.CostMatrix:
-            for col in row:
-                if type(col) is AirPort:
-                    print(0, end=' ')
-                else:
-                    print(col, end=' ')
-            print('')
+            print(row)
 
-    def SearchAP(self, NameAP: str) -> Union[tuple[str, Any], str]:
-        for i in range(len(self._ListAP)):
-            if self.CostMatrix[i][i].AirportName == NameAP:
+    def SearchAP(self, NameAP: str) -> Union[tuple[str, list], str]:
+        for i in range(len(self._ListAirports)):
+            if self._ListAirports[i].AirportName == NameAP:
                 adjacencyList: list = list(self.CostMatrix[i])  # Modify the adjacency list of AirPort i
-                adjacencyList[i] = 0
-                return (f"Founded ID: {self.CostMatrix[i][i].AirportId} Name: {self.CostMatrix[i][i].AirportName}",
+                return (f"Founded ID: {self._ListAirports[i].AirportId} Name: {self._ListAirports[i].AirportName}",
                         adjacencyList)
         else:
             return f"Can not find the airport with name {NameAP}"
 
-    def _Search(self, NameAP: str) -> int: # Private Search for name and return the index of the Airport
-        for i in range(len(self._ListAP)):
-            if self.CostMatrix[i][i].AirportName == NameAP:
-                return i
+    def _Search(self, NameAP: str) -> int:  # Private Search for name and return the index of the Airport
+        for i in range(len(self._ListAirports)):
+            if self._ListAirports[i].AirportName == NameAP:
+                return i  # return index of AP
         else:
-            return -1
+            return -1  # return -1 If the Airport does not exist
 
     def CostCal(self):
         pass
@@ -66,7 +62,9 @@ class AirportManager:
     def DelAP(self, APName):
         index = self._Search(APName)
         if index != -1:
-            del (self.CostMatrix[index])
+            self._ListAPId.remove(self._ListAirports[index].AirportId)
+            del (self._ListAirports[index])
+            del (self.CostMatrix[index])  # Delete in set
             for row in self.CostMatrix:
                 del (row[index])
 
@@ -82,6 +80,7 @@ def main():
     Manager.DisplayAP()
     A = Manager.SearchAP("Tuyet Voi 4")
     Manager.DelAP('Tuyet Voi')
+    Manager.DelAP('Tuyet Voi 2')
     print(f"After delete airport")
     Manager.DisplayAP()
     print(A)
