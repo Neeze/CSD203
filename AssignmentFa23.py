7013a8070e49a0efe7a5d0e9a017b9f7fd43d9f0
TuanTony5 <phutuan05122004@gmail.com>
2023-10-31 01:21:01 +0700

    31/10

--- AssignmentFa23.py
+++ AssignmentFa23.py
@@ -8,7 +8,9 @@ import numpy as np
     Quy ước:
         A[i][j] là hàng i, cột j. Lưu cost từ airport ID=i tới airport ID=j
 """
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 class AirPort:
     def __init__(self, AirportId: int, AirportName: str):
@@ -40,24 +42,25 @@ class AirportManager:
         """
         return np.zeros((capacity, capacity))
 
-    def upsize_array(self, capacity):
+    def resize_array(self):
         """
         check before insert
         Upsize if countV = maxV
         upsize x2 if the matrix full
         """
-        New_array = self.create_array(2 * capacity)
-        Old_array = self.Matrix
-        New_array[:self.countV, :self.countV] = Old_array
-        self.Matrix = New_array
-        self.maxV = 2 * capacity
-
+        if self.countV == self.maxV:
+            New_array = self.create_array(2 * self.maxV)
+            Old_array = self.Matrix
+            New_array[:self.countV, :self.countV] = Old_array
+            self.Matrix = New_array
+            self.maxV = 2 * self.maxV
+            return True
+        return False
 
     def addAP(self, Airport_Name):
         # Check capacity
         self.countV += 1
-        if self.countV == self.maxV:
-            self.upsize_array(self.maxV)
+        self.resize_array()
 
         # Add Airport to Linked List
         new_airport = AirPort(self.countV - 1, Airport_Name)  # Vì matrix start từ 0
@@ -67,8 +70,9 @@ class AirportManager:
             self.last.next = new_airport
         self.last = new_airport
 
-        #Add weight to another airports
-        if self.countV == 1:    return # Don't need to input if only one airport
+        # Add weight to another airports
+        if self.countV == 1:
+            return  # Don't need to input if only one airport
         else:
             new_id = new_airport.AirportId
 
@@ -87,20 +91,59 @@ class AirportManager:
     def display_Matrix(self):
         print(self.Matrix)
 
-    def searchAP(self,name):
+    def searchAP(self, name):
         airport = self.head
         while airport:
             if name in airport.AirportName:
                 airport.show_airport()
+            airport = airport.next
 
+    def dfs(self,id):
+        pass
     def costCal(self):
-        pass
+        # Create a list contain ID for visit DFS
+        dd = np.zeros((self.countV,))
 
-    def updateAP(self):
-        pass
+    def updateAP(self, id_update):
+        airport = self.head
+        state_tus = 0
+        # Update Airport Name
+        while airport:
+            if airport.AirportId == id_update:
+                airport.AirportName = input(f"New Airport Name for ID {id_update}: ")
+                state_tus = 1
+                break
+            airport = airport.next
+        if state_tus == 0: return False
+        # Update new weight
+        self.Matrix[id_update, :self.countV] = np.random.randint(10, 99, (1, self.countV))
+        self.Matrix[:self.countV, id_update] = np.random.randint(10, 99, (1, self.countV))
+        self.Matrix[id_update, id_update] = 0
+        return True
 
-    def delAP(self):
-        pass
+    def change_Linkedlist(self, id_del):
+        t = self.head
+        while t.AirportId != id_del:
+            t = t.next
+        t.AirportName = self.last.AirportName
+
+        while t.next != self.last:
+            t = t.next
+        self.last = None
+        self.last = t
+        t.next = None
+
+    def delAP(self, id_del):
+        if id_del >= self.countV or id_del < 0:
+            return False
+        self.countV -= 1
+        # delete weight related to airport_delete
+        self.Matrix[id_del, :] = self.Matrix[self.countV, :]
+        self.Matrix[:, id_del] = self.Matrix[:, self.countV]
+        self.Matrix[self.countV, :] = 0
+        self.Matrix[:, self.countV] = 0
+        # Change the new information for the delete id
+        self.change_Linkedlist(id_del)
 
 
 def main():
@@ -114,7 +157,13 @@ def main():
     A.addAP('Tokyo')
     A.displayAP()
     A.display_Matrix()
-    print(A.countV,A.maxV)
+    A.delAP(3)
+    A.displayAP()
+    A.display_Matrix()
+    A.updateAP(4)
+    A.displayAP()
+    A.display_Matrix()
+
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
 a = np.zeros((5,5))
-a[:,1] = np.random.randint(10,99,(1,5))
-a[1,1] = 0
-print(a)
+dd = np.zeros((6,))
+dd[1] = 1
+print(dd)
