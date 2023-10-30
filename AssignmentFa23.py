c4dd69be4cdfcb2cd2af3eabf2f765f92c4aec3b
Neeze <60569049+Neeze@users.noreply.github.com>
2023-10-30 10:02:52 +0700

    add Delete Ap

--- AssignmentFa23.py
+++ AssignmentFa23.py
@@ -3,9 +3,10 @@ from typing import Union, Any
 
 
 class AirPort:
-    def __init__(self, AirportId: int, AirportName: str):
+    def __init__(self, AirportId: int, AirportName: str, Location: str = None):
         self.AirportId = AirportId
         self.AirportName = AirportName
+        self.Location = Location
 
 
 class AirportManager:
@@ -35,7 +36,8 @@ class AirportManager:
             for col in row:
                 if type(col) is AirPort:
                     print(0, end=' ')
-                else: print(col, end=' ')
+                else:
+                    print(col, end=' ')
             print('')
 
     def SearchAP(self, NameAP: str) -> Union[tuple[str, Any], str]:
@@ -48,14 +50,25 @@ class AirportManager:
         else:
             return f"Can not find the airport with name {NameAP}"
 
+    def _Search(self, NameAP: str) -> int: # Private Search for name and return the index of the Airport
+        for i in range(len(self._ListAP)):
+            if self.CostMatrix[i][i].AirportName == NameAP:
+                return i
+        else:
+            return -1
+
     def CostCal(self):
         pass
 
     def UpdateAP(self):
         pass
 
-    def DelAP(self):
-        pass
+    def DelAP(self, APName):
+        index = self._Search(APName)
+        if index != -1:
+            del (self.CostMatrix[index])
+            for row in self.CostMatrix:
+                del (row[index])
 
 
 def main():
@@ -63,10 +76,15 @@ def main():
     Manager.AddAP(0, 'Tuyet Voi')
     Manager.AddAP(1, 'Tuyet Voi 1')
     Manager.AddAP(2, 'Tuyet Voi 2')
+    Manager.AddAP(3, 'Tuyet Voi 3')
+    Manager.AddAP(4, 'Tuyet Voi 4')
+    Manager.AddAP(5, 'Tuyet Voi 5')
     Manager.DisplayAP()
     A = Manager.SearchAP("Tuyet Voi 4")
+    Manager.DelAP('Tuyet Voi')
+    print(f"After delete airport")
+    Manager.DisplayAP()
     print(A)
-    pass
 
 
 if __name__ == "__main__":
